64b6e1de4049902569ae5e9dfa837c707da9267a
Caleb Hamernick <caleb.hamernick@gmail.com>
2022-02-23 16:51:37 -0500

    commit tree ec5a131bb1ad75317f28cdedeb310388dcecf623

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  
+  let message;
+  if (distance <= 400) {
+    message = 'This one is on me!';
+  } else { (distance <= 2000) 
+    message = 'I will gladly take your thirty bucks.';
+ // } else { (distance <= 2500) 
+ //   message = 'No can do.';
+  }
+  return  message
   // Write your code here!
 }
 
 function ternaryCheckCity(){
   // Write your code here!
-}
+  let message1 = "Ok, sounds good." ? "NYC" : "false"}
 
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+
+
